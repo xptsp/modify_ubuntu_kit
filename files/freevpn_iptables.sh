f8bf0dd6c2847b8e2201d3aff5b4d253eb675dda
Douglas Orend <douglas.orend@comcast.net>
2020-01-18 16:16:24 -0600

    Split Tunnel VPN fixes

--- files/freevpn_iptables.sh
+++ files/freevpn_iptables.sh
@@ -7,7 +7,12 @@ export VPNUSER=htpc
 ETHERNET=$(lshw -c network -disable usb -short | grep -i "ethernet")
 IFS=" " read -ra ETH_NAME <<< $ETHERNET
 export NETIF=${ETH_NAME[1]}
-export LOCALIP=$(ip address show $NETIF | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | tail -n1)
+export LOCALIP=$(ip address show $NETIF | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | tail -n1)/16
+
+export PORTS=($([[ -f /etc/openvpn/freevpn/port_forward.list ]] && cat /etc/openvpn/freevpn/port_forward.list))
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sysctl -w net.ipv4.ip_forward=1
 
 # flushes all the iptables rules, if you have other rules to use then add them into the script
 iptables -F -t nat
@@ -23,7 +28,13 @@ iptables -t mangle -A OUTPUT ! --src $LOCALIP -j MARK --set-mark 0x1
 iptables -t mangle -A OUTPUT -j CONNMARK --save-mark
 
 # allow responses
-iptables -A INPUT -i $INTERFACE -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# allow incoming mapped port from AirVPN
+for $PORT in $PORTS; do
+	iptables -A INPUT -i $INTERFACE -p tcp --dport $PORT -j ACCEPT
+	iptables -A INPUT -i $INTERFACE -p udp --dport $PORT -j ACCEPT
+done
 
 # block everything incoming on $INTERFACE to prevent accidental exposing of ports
 iptables -A INPUT -i $INTERFACE -j REJECT
@@ -35,10 +46,23 @@ iptables -A OUTPUT -o $INTERFACE -m owner --uid-owner $VPNUSER -j ACCEPT
 # all packets on $INTERFACE needs to be masqueraded
 iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
 
+# setup each port in the "port_forward.list":
+for PORT in "${PORTS[@]}"; do
+	# forward mapped ports
+	iptables -t nat -A PREROUTING -p tcp -i $INTERFACE --dport $PORT -j DNAT --to $LOCALIP
+	iptables -t nat -A PREROUTING -p udp -i $INTERFACE --dport $PORT -j DNAT --to $LOCALIP
+	iptables -A FORWARD -p tcp -i $INTERFACE -o $NETIF -d $LOCALIP --dport $PORT -j ACCEPT
+	iptables -A FORWARD -p udp -i $INTERFACE -o $NETIF -d $LOCALIP --dport $PORT -j ACCEPT
+
+	# allow output (to masqueraded address) from mapped port from port
+	iptables -A OUTPUT -o $NETIF -p tcp --sport $PORT -j ACCEPT
+	iptables -A OUTPUT -o $NETIF -p udp --sport $PORT -j ACCEPT
+done
+
 # reject connections from predator IP going over $NETIF
 iptables -A OUTPUT ! --src $LOCALIP -o $NETIF -j REJECT
 
 # Start routing script
-. /etc/openvpn/freevpn/freevpn_routing.sh
+/etc/openvpn/freevpn/freevpn_routing.sh
 
 exit 0
--- files/freevpn_login.sh
+++ files/freevpn_login.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$EUID" -ne 0 ]]; then
-    sudo $0
-    exit
+	sudo $0
+	exit
 fi
 export RED='\033[1;31m'
 export BLUE='\033[1;34m'
@@ -11,17 +11,17 @@ export NC='\033[0m'
 url='https://freevpn.co.uk/accounts/'
 html=$( curl -# -L "${url}" 2> '/dev/null' )
 if [[ -z ${html} ]]; then
-    echo -e "${RED}ERROR:${NC} Cannot get FreeVPN account page from the internet!"
-    echo "Make sure you are connected to the internet before trying again!"
-    exit 1
+	echo -e "${RED}ERROR:${NC} Cannot get FreeVPN account page from the internet!"
+	echo "Make sure you are connected to the internet before trying again!"
+	exit 1
 fi
 
 # Parse html to get username and password:
 username=$(echo ${html} | grep -P -o -e '(<b>Username:</b> (.*?)<\/li>)' | head -n 1 | cut -d">" -f 3 | cut -d"<" -f 1 | cut -d" " -f 2)
 password=$(echo ${html} | grep -P -o -e '(<b>Password:</b> (.*?)<\/li>)' | head -n 1 | cut -d">" -f 3 | cut -d"<" -f 1 | cut -d" " -f 2)
 if [[ -z ${username} || -z ${password} ]]; then
-    echo -e "${RED}ERROR:${NC} Error getting FreeVPN credentials!  Aborting!"
-    exit 2
+	echo -e "${RED}ERROR:${NC} Error getting FreeVPN credentials!  Aborting!"
+	exit 2
 fi
 echo -e "${BLUE}Username:${NC} ${username}"
 echo -e "${BLUE}Password:${NC} ${password}"
@@ -33,31 +33,32 @@ echo -e "${BLUE}Current Certificate:${NC} ${this_update:-"N/A"}"
 last_update=$(echo ${html} | grep -P -o -e '(Updated ([^<].)*\.)')
 echo -e "${BLUE}Certificates Last Updated:${NC} ${last_update}"
 if [[ ! "${last_update}" == "${this_update}" ]]; then
-    # Get the certificates URL from the accounts page.  Abort at any point if expected result isn't there:
-    url=$(echo ${html} | grep -P -o -e '(https://freevpn.me/FreeVPN.me-OpenVPN-Bundle-.*.zip)')
-    if [[ -z ${url} ]]; then
-        echo -e "${RED}ERROR:${NC} Error getting FreeVPN certificate URL!  Aborting!"
-        exit 3
-    fi
-    base=$(basename ${url})
-    wget ${url} -O /tmp/${base} >& /dev/null 2>&1
-    if [[ ! -f /tmp/${base} ]]; then
-        echo -e "${RED}ERROR:${NC} Error getting FreeVPN certificates from website!  Aborting!"
-        exit 4
-    fi
-    7z x /tmp/${base} -aoa -o/tmp/freevpn.me >& /dev/null 2>&1
-    rm /tmp/${base}
-    file="/tmp/freevpn.me/$(echo ${base} | sed "s|.zip||g")/6 - FreeVPN.co.uk - DE/FreeVPN.co,uk-TCP443.ovpn"
-    if [[ ! -f "${file}" ]]; then
-        echo -e "${RED}ERROR:${NC} Cannot find required certificate file inside downloaded certificate pack!  Aborting!"
-        exit 5
-    fi
+	# Get the certificates URL from the accounts page.  Abort at any point if expected result isn't there:
+	url=$(echo ${html} | grep -P -o -e '(https://freevpn.me/FreeVPN.me-OpenVPN-Bundle-.*.zip)')
+	if [[ -z ${url} ]]; then
+		echo -e "${RED}ERROR:${NC} Error getting FreeVPN certificate URL!  Aborting!"
+		exit 3
+	fi
+	base=$(basename ${url})
+	wget ${url} -O /tmp/${base} >& /dev/null 2>&1
+	if [[ ! -f /tmp/${base} ]]; then
+		echo -e "${RED}ERROR:${NC} Error getting FreeVPN certificates from website!  Aborting!"
+		exit 4
+	fi
+	7z x /tmp/${base} -aoa -o/tmp/freevpn.me >& /dev/null 2>&1
+	rm /tmp/${base}
+	file=$(find /tmp/freevpn.me | grep -e "uk-TCP443")
+	echo -e "${BLUE}Using OVPN file:${NC} $(basename "${file}")"
+	if [[ ! -f "${file}" ]]; then
+		echo -e "${RED}ERROR:${NC} Cannot find required certificate file inside downloaded certificate pack!  Aborting!"
+		exit 5
+	fi
 
-    # Modify the certificate to meet our needs:
-    cat "${file}" | egrep -v "(auth-user-pass|script-security|block-outside-dns)" > /etc/openvpn/freevpn/freevpn.conf
-    sed -i "s|;comp-lzo|comp-lzo|g" /etc/openvpn/freevpn/freevpn.conf
-    sed -i "s|dev tun|dev vpn_out\ndev-type tun|g" /etc/openvpn/freevpn/freevpn.conf
-    cat << EOF >> /etc/openvpn/freevpn/freevpn.conf
+	# Modify the certificate to meet our needs:
+	cat "${file}" | egrep -v "(auth-user-pass|script-security|block-outside-dns)" > /etc/openvpn/freevpn/freevpn.conf
+	sed -i "s|;comp-lzo|comp-lzo|g" /etc/openvpn/freevpn/freevpn.conf
+	sed -i "s|dev tun|dev vpn_out\ndev-type tun|g" /etc/openvpn/freevpn/freevpn.conf
+	cat << EOF >> /etc/openvpn/freevpn/freevpn.conf
 
 #user authorization stuff:
 auth-user-pass /etc/openvpn/freevpn/freevpn_creds
@@ -73,5 +74,5 @@ down-pre
 # prevent DNS leakage
 dhcp-option DOMAIN-ROUTE .
 EOF
-    echo ${last_update} > /etc/openvpn/freevpn/freevpn_last_update
+	echo ${last_update} > /etc/openvpn/freevpn/freevpn_last_update
 fi
