71df9afd4882636d31b85475ec62df7782112f52
Douglas Orend <douglas.orend@comcast.net>
2020-03-31 09:35:25 -0500

    Update 50_lirc.sh

--- files/tasks.d/50_lirc.sh
+++ files/tasks.d/50_lirc.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Try to reconfigure LIRC to use Microsoft RC6 remote:
 export DEBIAN_FRONTEND=noninteractive
 echo "lirc lirc/dev_input_device select /dev/lirc0" | debconf-set-selections
 echo "lirc lirc/transmitter select None" | debconf-set-selections
 echo "lirc lirc/serialport select /dev/ttyS0" | debconf-set-selections
 echo "lirc lirc/remote select Windows Media Center Transceivers/Remotes (all)" | debconf-set-selections
 dpkg-reconfigure -u lirc
+
+# Add $USERNAME to the dialout group:
+usermod -aG dialout ${USERNAME:-"${USER}"}
