3c21ced5366ab645a63e209a21e90fc3caaa794d
Douglas Orend <douglas.orend@comcast.net>
2020-04-21 09:01:48 -0500

    Update vpn_iptables.sh

--- files/vpn_iptables.sh
+++ files/vpn_iptables.sh
@@ -10,12 +10,11 @@ ETHERNET=$(lshw -c network -disable usb -short | grep -i "ethernet")
 IFS=" " read -ra ETH_NAME <<< $ETHERNET
 export NETIF=${ETH_NAME[1]}
 [[ "$1" == "-v" ]] && echo "Ethernet Interface = $NETIF"
-export LOCALIP=$(ip address show $NETIF | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | tail -n1)
+export LOCALIP=$(ip address show $NETIF | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | tail -n1)/16
 [[ "$1" == "-v" ]] && echo "Ethernet IP Address = $LOCALIP"
 
 # Determine what the interface name is and IP address:
-[[ -f /etc/openvpn/freevpn/freevpn.conf ]] && export INTERFACE=$(cat /etc/openvpn/freevpn/freevpn.conf | grep "dev " | cut -d" " -f 2)
-[[ -z "$INTERFACE" ]] && export INTERFACE=$(cat /etc/openvpn/vpn.conf | grep "dev " | cut -d" " -f 2)
+export INTERFACE=$(cat /etc/openvpn/vpn.conf | grep "dev " | cut -d" " -f 2)
 [[ "$1" == "-v" ]] && echo "VPN Interface = $INTERFACE"
 export GATEWAYIP=$(ip address show $INTERFACE | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | tail -n1)
 [[ "$1" == "-v" ]] && echo "VPN IP Address = $GATEWAYIP"
@@ -36,6 +35,14 @@ iptables -t mangle -A OUTPUT -j CONNMARK --save-mark
 # allow responses
 iptables -A INPUT -i $INTERFACE -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
+# allow incoming mapped port from allowed ports
+if [[ ! -z "${PORTS[@]}" ]]; then
+	for PORT in "${PORTS[@]}"; do
+		iptables -A INPUT -i $INTERFACE -p tcp --dport $PORT -j ACCEPT
+		iptables -A INPUT -i $INTERFACE -p udp --dport $PORT -j ACCEPT
+	done
+fi
+
 # block everything incoming on $INTERFACE to prevent accidental exposing of ports
 iptables -A INPUT -i $INTERFACE -j REJECT
 
@@ -45,6 +52,21 @@ iptables -A OUTPUT -o $INTERFACE -m owner --uid-owner $VPNUSER -j ACCEPT
 
 # all packets on $INTERFACE needs to be masqueraded
 iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
+
+# setup each port in the "port_forward.list":
+if [[ ! -z "${PORTS[@]}" ]]; then
+	for PORT in "${PORTS[@]}"; do
+		# forward mapped ports
+		iptables -t nat -A PREROUTING -p tcp -i $INTERFACE --dport $PORT -j DNAT --to $LOCALIP
+		iptables -t nat -A PREROUTING -p udp -i $INTERFACE --dport $PORT -j DNAT --to $LOCALIP
+		iptables -A FORWARD -p tcp -i $INTERFACE -o $NETIF -d $LOCALIP --dport $PORT -j ACCEPT
+		iptables -A FORWARD -p udp -i $INTERFACE -o $NETIF -d $LOCALIP --dport $PORT -j ACCEPT
+
+		# allow output (to masqueraded address) from mapped port from port
+		iptables -A OUTPUT -o $NETIF -p tcp --sport $PORT -j ACCEPT
+		iptables -A OUTPUT -o $NETIF -p udp --sport $PORT -j ACCEPT
+	done
+fi
 
 # reject connections from predator IP going over $NETIF
 iptables -A OUTPUT ! --src $LOCALIP -o $NETIF -j REJECT
