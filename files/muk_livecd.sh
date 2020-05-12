5d391787a46d9868f945aa0fb00fa835cba93f07
Douglas Orend <douglas.orend@comcast.net>
2020-05-12 11:32:57 -0500

    Update muk_livecd.sh

--- files/muk_livecd.sh
+++ files/muk_livecd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 C=/cdrom/casper/filesystem-opt
-if [[ -f ${C}.squashfs && -f ${C}.location ]]; then
+if [[ -f ${C}.packed_alt && -f ${C}.location ]]; then
 	mount ${C}.packed_alt /$(cat ${C}.location)
-	mount ${C}.packed_alt /rofl/$(cat ${C}.location)
+	mount ${C}.packed_alt /rofs/$(cat ${C}.location)
 fi
