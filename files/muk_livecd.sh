3719b8e612181016c47ea90a64b41dab50717542
Douglas Orend <douglas.orend@comcast.net>
2020-05-12 12:27:58 -0500

    Update muk_livecd.sh

--- files/muk_livecd.sh
+++ files/muk_livecd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 C=/cdrom/casper/filesystem-opt
+D=$(cat ${C}.location)
 [[ -f ${C}.squashfs && -f ${C}.location ]] && cat << EOF >> /etc/fstab
-${C}.packed_alt  /$(cat ${C}.location  auto  defaults,nofail  0  0
-${C}.packed_alt  /rofs/$(cat ${C}.location  auto  defaults,nofail  0  0
+${C}.packed_alt  /${D}       auto  defaults,nofail  0  0
+${C}.packed_alt  /rofs/${D}  auto  defaults,nofail  0  0
 EOF
