ac3f9a175ebdf6b37063e3c167bcb7978bd31bd6
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2020-06-30 09:11:28 -0500

    Update 80_docker.sh

--- files/tasks.d/80_docker.sh
+++ files/tasks.d/80_docker.sh
@@ -4,3 +4,7 @@ if ! cat /etc/fstab | grep "/var/lib/docker" >& /dev/null; then
 	[[ ! -d /var/lib/docker ]] && mkdir -p /var/lib/docker
 	echo "/home/docker  /var/lib/docker  none  bind  0  0" >> /etc/fstab
 fi
+
+[[ -z "${USERNAME}" ]] && USERNAME=$(id -un 1000)
+usermod -aG docker ${USERNAME}
+usermod -aG docker htpc
