e3213fe3c264e983c838a83cd83714c88d89fa9b
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2020-06-30 09:00:17 -0500

    Update docker.sh

--- desktop/docker.sh
+++ desktop/docker.sh
@@ -14,7 +14,7 @@ fi
 #==============================================================================
 _title "Installing Docker..."
 #==============================================================================
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
@@ -25,3 +25,9 @@ if ischroot; then
 else
 	${MUK_DIR}/files/tasks.d/80_docker.sh
 fi
+
+#==============================================================================
+_title "Installing Docker Compose..."
+#==============================================================================
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
