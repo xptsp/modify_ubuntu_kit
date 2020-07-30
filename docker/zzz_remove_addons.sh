7379767692fbe9107c56b949bf168c6fad55f6d7
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2020-07-30 11:34:11 -0500

    Create zzz_remove_addons.sh

---
+++ docker/zzz_remove_addons.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+[[ -f /usr/local/finisher/settings.conf ]] && . /usr/local/finisher/settings.conf
+MUK_DIR=${MUK_DIR:-"/opt/modify_ubuntu_kit"}
+[[ ! -e ${MUK_DIR}/files/includes.sh ]] && (echo Missing includes file!  Aborting!; exit 1)
+. ${MUK_DIR}/files/includes.sh
+
+# No parameter specified?  Or maybe help requested?
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+	echo -e "${RED}Purpose:${NC} Remvoes Kodi addons."
+	echo ""
+	exit 0
+fi
+
+#==============================================================================
+_title "Remvoes Kodi addons...."
+#==============================================================================
+rm -rf /opt/kodi
+rm -rf ~/.kodi/addons
