989364def16500d19471c2a914909717726f0e6e
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2024-02-03 18:52:59 -0600

    Update gnome-extensions.sh

--- base/gnome-extensions.sh
+++ base/gnome-extensions.sh
@@ -73,8 +73,24 @@ EOF
 chmod +x /usr/local/bin/gnome-ext-install
 
 #==============================================================================
+_title "Adding polkit for Hibernation option..."
+#==============================================================================
+cat << EOF > /etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla
+[Re-enable hibernate by default in upower]
+Identity=unix-user:*
+Action=org.freedesktop.upower.hibernate
+ResultActive=yes
+
+[Re-enable hibernate by default in logind]
+Identity=unix-user:*
+Action=org.freedesktop.login1.hibernate;org.freedesktop.login1.handle-hibernate-key;org.freedesktop.login1;org.freedesktop.login1.hibernate-multiple-sessions;org.freedesktop.login1.hibernate-ignore-inhibit
+ResultActive=yes
+EOF
+
+#==============================================================================
 _title "Installing some Gnome Extensions..."
 #==============================================================================
+apt install -y chrome-gnome-shell
 mkdir /tmp/tmp
 cd /tmp/tmp
 
