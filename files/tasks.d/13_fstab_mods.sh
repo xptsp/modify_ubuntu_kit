1342bbed3c1c4573eae87a0eed389ce57a111682
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2024-02-15 12:57:18 -0600

    Update 13_fstab_mods.sh

--- files/tasks.d/13_fstab_mods.sh
+++ files/tasks.d/13_fstab_mods.sh
@@ -8,37 +8,48 @@ tmpfs /tmp tmpfs defaults 0 0
 DONE
 
 # If my Windows partition is present, add it to "/etc/fstab":
-if blkid | grep -q "\"A4A4659DA465732A\""; then
+DEV=$(grep "Windows Boot Manager" /boot/grub/grub.cfg | awk '{print $6}' | cut -d\) -f 1)
+UUID=$(blkid -o export $DEV | grep "^UUID=" | cut -d= -f 2)
+if [[ ! -z "$UUID" ]]; then
 	mkdir -p /mnt/Windows
-	sed -i "/UUID=A4A4659DA465732A/d" $F
-	echo "UUID=A4A4659DA465732A /mnt/Windows ntfs rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
+	sed -i "/\/mnt\/Windows/d" $F
+	echo "UUID=${UUID} /mnt/Windows ntfs rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
+fi
+
+# If a partition with the username of user 1000 plus "_Public", mount it to "/home/{USER}/Public" in "/etc/fstab":
+USER=$(grep ":1000:" /etc/passwd | cut -d: -f 1)
+DEV=$(blkid | grep -i "\"${USER}_Public\"" | cut -d: -f 1)
+UUID=$(blkid -o export $DEV | grep "^UUID=" | cut -d= -f 2)
+if [[ -z "$UUID" ]]; then
+	sed -i "/\/home\/doug\/Public/d" $F
+	echo "UUID=${UUID} /home/doug/Public ntfs noatime,rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
+	sed -i "/\/home\/doug\/Public\/Downloads/d" $F
+	echo "/home/doug/Public/Downloads /home/doug/Downloads none bind 0 0" >> $F
+fi
+
+# If a partition with the username of user 1000 plus "_Documents", mount it to "/home/{USER}/Documents" in "/etc/fstab":
+DEV=$(blkid | grep -i "\"${USER}_Documents\"" | cut -d: -f 1)
+UUID=$(blkid -o export $DEV | grep "^UUID=" | cut -d= -f 2)
+if [[ -z "$UUID" ]]; then
+	sed -i "/\/home\/doug\/Documents/d" $F
+	echo "UUID=${UUID} /home/doug/Documents ext4 defaults,noatime 0 0" >> $F
+	sed -i "/\/home\/doug\/Documents\/GitHub/d" $F
+	echo "/home/doug/Documents/GitHub /home/doug/GitHub none bind 0 0" >> $F
+	echo "/home/doug/GitHub/modify_ubuntu_kit  /opt/modify_ubuntu_kit  none bind 0 0" >> $F
 fi
 
 # If my Ubuntu USB installation stick is present, add it to "/etc/fstab":
 if blkid | grep -q "\"b72b7891-f821-42bb-b457-8a3878e8a46a\""; then
-	mkdir -p /img/usb_{casper,live}
+	mkdir -p /img
+	echo "/home/img /img none bind 0 0" >> $F
+	mkdir -p /home/img/usb_{casper,live}
 	sed -i "/UUID=b72b7891-f821-42bb-b457-8a3878e8a46a/d" $F
-	echo "UUID=b72b7891-f821-42bb-b457-8a3878e8a46a /img/usb_casper ext4 noatime,defaults,noatime 0 0" >> $F
+	echo "UUID=b72b7891-f821-42bb-b457-8a3878e8a46a /home/img/usb_casper ext4 noatime,defaults,noatime 0 0" >> $F
 	sed -i "/UUID=C198-307D/d" $F
-	echo "UUID=C198-307D /img/usb_live vfat noatime,rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
+	echo "UUID=C198-307D /home/img/usb_live vfat noatime,rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
 	mkdir -p /img/mnt
 	sed -i "/^unionfs\#/d" $F
-	echo "unionfs#/img/usb_casper=rw:/img/usb_live=rw /img/mnt fuse default_permissions,allow_other,use_ino,nonempty,suid,cow 0 0" >> $F
-fi
-
-# If my external hard drive is present, add it to "/etc/fstab":
-if blkid | grep -q "\"1109A7497BFE26A3\""; then
-	sed -i "/UUID=1109A7497BFE26A3/d" $F
-	echo "UUID=1109A7497BFE26A3 /home/doug/Public ntfs noatime,rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
-	sed -i "/\/home\/doug\/Public\/Downloads/d" $F
-	echo "/home/doug/Public/Downloads /home/doug/Downloads none bind 0 0" >> $F
-fi
-if blkid | grep -q "\"076ae757-89b6-4f2c-ae64-391069c9d942\""; then
-	sed -i "/UUID=076ae757-89b6-4f2c-ae64-391069c9d942/d" $F
-	echo "UUID=076ae757-89b6-4f2c-ae64-391069c9d942 /home/doug/Documents ext4 defaults,noatime 0 0" >> $F
-	sed -i "/\/home\/doug\/Documents\/GitHub/d" $F
-	echo "/home/doug/Documents/GitHub /home/doug/GitHub none bind 0 0" >> $F
-	echo "/home/doug/GitHub/modify_ubuntu_kit  /opt/modify_ubuntu_kit  none bind 0 0" >> $F
+	echo "unionfs#/home/img/usb_casper=rw:/home/img/usb_live=rw /home/img/mnt fuse default_permissions,allow_other,use_ino,nonempty,suid,cow 0 0" >> $F
 fi
 
 # Pretify the "/etc/fstab" file, then mount everything:
