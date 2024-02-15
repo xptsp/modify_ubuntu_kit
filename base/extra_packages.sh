3e7557ca2d29f7774bb9ac1a6239e243ebbb3f7a
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2024-02-15 14:00:49 -0600

    Update 13_fstab_mods.sh

--- base/extra_packages.sh
+++ base/extra_packages.sh
@@ -16,5 +16,5 @@ _title "Installing some extra packages on your machine..."
 #==============================================================================
 [[ "$(cat /etc/os-release | grep "VERSION_ID=" | cut -d"\"" -f 2 | cut -d" " -f 1)" != "22.04" ]] && apt install -y ttf-ubuntu-font-family
 apt install -y dh-modaliases build-essential dkms dpkg-dev debhelper checkinstall
-apt install -y p7zip-full p7zip-rar rar unrar net-tools rename tree squashfs-tools git genisoimage dialog
+apt install -y p7zip-full p7zip-rar rar unrar net-tools rename tree squashfs-tools git genisoimage dialog unionfs-fuse
 cc ${MUK_DIR}/files/usbreset.c -o /usr/local/bin/usbreset
--- files/tasks.d/13_fstab_mods.sh
+++ files/tasks.d/13_fstab_mods.sh
@@ -7,25 +7,21 @@ echo "tmpfs /tmp tmpfs defaults 0 0" >> $F
 
 # Bind "/home/img" to "/img":
 mkdir -p /home/img
-sed -i "/^\/home\/img/d" $F
+sed -i "/\/home\/img/d" $F
 echo "/home/img /img none bind 0 0" >> $F
 
-# If my Windows partition is present, add it to "/etc/fstab":
-DEV=$(grep "Windows Boot Manager" /boot/grub/grub.cfg | awk '{print $6}' | cut -d\) -f 1)
-UUID=$(blkid -o export $DEV | grep "^UUID=" | cut -d= -f 2)
-if [[ ! -z "$UUID" ]]; then
-	mkdir -p /mnt/Windows
-	sed -i "/\/mnt\/Windows/d" $F
-	echo "UUID=${UUID} /mnt/Windows ntfs rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
-fi
+# Default options per filesystem type:
+declare -A FS=()
+FS[ntfs]=noatime,rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022
+FS[vfat]=${ntfs}
 
 # If a partition with the username of user 1000 plus "_Public", mount it to "/home/{USER}/Public" in "/etc/fstab":
 USER=$(grep ":1000:" /etc/passwd | cut -d: -f 1)
 DEV=$(blkid | grep -i "\"${USER}_Public\"" | cut -d: -f 1)
-UUID=$(blkid -o export $DEV | grep "^UUID=" | cut -d= -f 2)
-if [[ ! -z "$UUID" ]]; then
+if [[ ! -z "$DEV" ]]; then
+	eval `blkid -o export ${DEV}`
 	sed -i "/\/home\/${USER}\/Public/d" $F
-	echo "UUID=${UUID} /home/${USER}/Public ntfs noatime,rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
+	echo "UUID=${UUID} /home/${USER}/Public ${TYPE} ${FS[$TYPE]:-"defaults,noatime"} 0 0" >> $F
 	mount /home/${USER}/Public
 	mkdir -p /home/${USER}/Public/Downloads
 	sed -i "/\/home\/${USER}\/Public\/Downloads/d" $F
@@ -35,9 +31,10 @@ fi
 # If a partition with the username of user 1000 plus "_Documents", mount it to "/home/{USER}/Documents" in "/etc/fstab":
 DEV=$(blkid | grep -i "\"${USER}_Documents\"" | cut -d: -f 1)
 UUID=$(blkid -o export $DEV | grep "^UUID=" | cut -d= -f 2)
-if [[ ! -z "$UUID" ]]; then
+if [[ ! -z "$DEV" ]]; then
+	eval `blkid -o export ${DEV}`
 	sed -i "/\/home\/${USER}\/Documents/d" $F
-	echo "UUID=${UUID} /home/${USER}/Documents ext4 defaults,noatime 0 0" >> $F
+	echo "UUID=${UUID} /home/${USER}/Documents ${TYPE} ${FS[$TYPE]:-"defaults,noatime"} 0 0" >> $F
 	mount /home/${USER}/Documents
 	mkdir -p /home/${USER}/Documents/GitHub
 	sed -i "/\/home\/${USER}\/Documents\/GitHub/d" $F
@@ -52,14 +49,14 @@ if blkid | grep -q "\"b72b7891-f821-42bb-b457-8a3878e8a46a\""; then
 	echo "/home/img /img none bind 0 0" >> $F
 	mkdir -p /home/img/usb_{casper,live}
 	sed -i "/UUID=b72b7891-f821-42bb-b457-8a3878e8a46a/d" $F
-	echo "UUID=b72b7891-f821-42bb-b457-8a3878e8a46a /home/img/usb_casper ext4 noatime,defaults,noatime 0 0" >> $F
+	echo "UUID=b72b7891-f821-42bb-b457-8a3878e8a46a /home/img/usb_casper ext4 defaults,noatime 0 0" >> $F
 	sed -i "/UUID=C198-307D/d" $F
-	echo "UUID=C198-307D /home/img/usb_live vfat noatime,rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0111,dmask=0022 0 0" >> $F
+	echo "UUID=C198-307D /home/img/usb_live vfat ${FS[vfat] 0 0" >> $F
 	mkdir -p /img/mnt
 	sed -i "/^unionfs\#/d" $F
 	echo "unionfs#/home/img/usb_casper=rw:/home/img/usb_live=rw /home/img/mnt fuse default_permissions,allow_other,use_ino,nonempty,suid,cow 0 0" >> $F
 fi
 
 # Pretify the "/etc/fstab" file, then mount everything:
-cat $F | grep -v "^#" | column -t | tee $F
+cat $F | grep -v "^#" | column -t | tee $F >& /dev/null
 mount -a
