fd5741f31bec19fbd650c9c6271e511f6fc8e6e0
Douglas Orend <douglas.orend@comcast.net>
2024-02-22 14:12:35 -0600

    Update nautilus-extensions.sh

--- base/nautilus-extensions.sh
+++ base/nautilus-extensions.sh
@@ -12,6 +12,18 @@ if [[ "$1" == "--help" || "$1" == "-h" ]]; then
 fi
 
 #==============================================================================
-_title "Installs Nautilus extensions and tweaks..."
+_title "Installs Nautilus extensions available in Ubuntu repo..."
 #==============================================================================
-apt install -y nautilus-admin 
+apt install -y nautilus-admin nautilus-dropbox nautilus-extension-brasero nautilus-filename-repairer nautilus-gtkhash nautilus-image-converter nautilus-hide nautilus-wipe folder-color  
+
+#==============================================================================
+_title "Installs other Nautilus extensions..."
+#==============================================================================
+
+# First: Nautilus Gnome Disks
+#==============================================================================
+DEST=/usr/share/nautilus-python/extensions/
+DIR=/tmp/nautilus-gnome-disks
+git clone https://github.com/thebitstick/nautilus-gnome-disks ${DIR}
+install --mode=644 ${DIR}/nautilus-gnome-disks.py ${DEST}/
+rm -rf ${DIR}
