6bbc34d0bff00d19cd7871bb53757efd5b8857b2
Douglas Orend <douglas.orend@comcast.net>
2024-02-25 16:46:31 -0600

    Removed snaps scripts

--- files/tasks.d/98_snaps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install any snaps and ack files found in "/var/lib/snapd/snaps.new": 
-for FILE in /var/lib/snapd/snaps.new/*.snap; do 
-	snap ack ${FILE/snap/assert}
-	snap install ${FILE}
-done
