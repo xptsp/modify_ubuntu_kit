17eff5da4d14f6c4f98ba459f3bbf1a53e4ab89f
Douglas Orend <douglas.orend@comcast.net>
2024-02-29 19:09:44 -0600

    It's the little things....

--- files/boot.d/13_fstab_mods.sh
+++ files/boot.d/13_fstab_mods.sh
@@ -17,6 +17,7 @@ HOME=$(grep -m 1 "^${USER}:" /etc/passwd | cut -d: -f 6)
 # If a partition with the username of user 1000 plus "_Public", mount it to "/home/{USER}/Public" in "/etc/fstab":
 DEV=$(blkid | grep -i "\"${USER}_Public\"" | cut -d: -f 1)
 if [[ ! -z "$DEV" ]]; then
+	umount ${DEV}
 	eval `blkid -o export ${DEV}`
 	sed -i "/\/home\/${USER}\/Public/d" $F
 	echo "UUID=${UUID} ${HOME}/Public ${TYPE} ${FS[$TYPE]:-"defaults,noatime"} 0 0" >> $F
@@ -27,6 +28,7 @@ fi
 # If a partition with the username of user 1000 plus "_Documents", mount it to "/home/{USER}/Documents" in "/etc/fstab":
 DEV=$(blkid | grep -i "\"${USER}_Documents\"" | cut -d: -f 1)
 if [[ ! -z "$DEV" ]]; then
+	umount ${DEV}
 	eval `blkid -o export ${DEV}`
 	sed -i "/\/home\/${USER}\/Documents/d" $F
 	echo "UUID=${UUID} ${HOME}/Documents ${TYPE} ${FS[$TYPE]:-"defaults,noatime"} 0 0" >> $F
--- files/firstboot.sh
+++ files/firstboot.sh
@@ -5,9 +5,9 @@ MUK_DIR=${MUK_DIR:-"/opt/modify_ubuntu_kit"}
 [[ ! -e "${MUK_DIR}" ]] && exit 0
 
 # If we are running off a live CD/USB, >>EXIT NOW<<!!!
-mount | grep -q "/cow " && exit 0 
+if mount | grep " / " | grep -q "/cow "; then echo "Running on Live CD/USB!  Aborting..."; exit 0; fi
 
 # Execute any firstboot scripts in the "/usr/local/finisher/firstboot.d" directory:
-test -f /usr/local/finisher/boot.d/*.sh && ls /usr/local/finisher/boot.d/*.sh | while read FILE; do ${FILE}; rm ${FILE}; done
+[[ $(ls /usr/local/finisher/boot.d/*.sh | wc -l) -ne 0 ]] && ls /usr/local/finisher/boot.d/*.sh | while read FILE; do ${FILE}; rm ${FILE}; done
 
 exit 0
