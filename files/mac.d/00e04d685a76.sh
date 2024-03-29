22c734d331b27c34832f63298db4deb7a6e8ee0b
Douglas Orend <douglas.orend@comcast.net>
2024-03-28 19:09:56 -0500

    Update 00e04d685a76.sh

--- files/mac.d/00e04d685a76.sh
+++ files/mac.d/00e04d685a76.sh
@@ -2,7 +2,7 @@
 # Notes: My personal machine
 cat << EOF >> /etc/pulse/default.pa 
 
-load-module module-alsa-sink device=hdmi:0 
+load-module module-alsa-sink device=hw:0,7 
 load-module module-combine-sink sink_name=combined
 set-default-sink combined
 EOF
