8e63b70214322fe08d0e0a41bfc3f2c270a5e2d1
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2025-03-21 07:19:58 -0500

    Create kdeconnect.sh

---
+++ misc/kdeconnect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+[[ -f /usr/local/finisher/settings.conf ]] && . /usr/local/finisher/settings.conf
+MUK_DIR=${MUK_DIR:-"/opt/modify_ubuntu_kit"}
+[[ ! -e ${MUK_DIR}/files/includes.sh ]] && (echo Missing includes file!  Aborting!; exit 1)
+. ${MUK_DIR}/files/includes.sh
+
+# No parameter specified?  Or maybe help requested?
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+	echo -e "${RED}Purpose:${NC} Installs KDEConnect on this computer."
+	echo ""
+	exit 0
+fi
+
+#==============================================================================
+_title "Installing KDEConnect..."
+#==============================================================================
+apt install -y kdeconnect
